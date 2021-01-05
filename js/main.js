b4575c53aa5082fc84f54134beb71f59d36f936a
Julian <41262691+SienceMC@users.noreply.github.com>
2021-01-05 14:40:33 +0100

    added Leaderboard Items

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 function game_click() {
-    alert("Coming soon!")
+    console.error("Coming soon!");
 }
 
 function coming_soon() {
-    alert("Coming soon!")
+    console.error("Coming soon!");
+}
+
+function leader_click(team_leader_id) {
+    console.error("Coming soon! Leader: " + team_leader_id);
 }
