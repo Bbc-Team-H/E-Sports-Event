ce25acb75b9f50d8a3d5e2bea4092053c1eb0be7
Julian <41262691+SienceMC@users.noreply.github.com>
2021-01-07 11:54:53 +0100

    started with container stuff

--- js/main.js
+++ js/main.js
@@ -13,3 +13,51 @@ function coming_soon() {
 function leader_click(team_leader_id) {
     console.error("Coming soon! Leader: " + team_leader_id);
 }
+
+function changeHome() {
+    homepage = document.querySelector("#desktop-home-container");
+    gamepage = document.querySelector("#desktop-game-container");
+    leaderboardpage = document.querySelector("#desktop-leaderboard-container");
+    teamspage = document.querySelector("#desktop-teams-container");
+
+    homepage.classList.remove("hidden");
+    gamepage.classList.add("hidden");
+    leaderboardpage.classList.add("hidden");
+    teamspage.classList.add("hidden");
+}
+
+function changeNextGame() {
+    homepage = document.querySelector("#desktop-home-container");
+    gamepage = document.querySelector("#desktop-game-container");
+    leaderboardpage = document.querySelector("#desktop-leaderboard-container");
+    teamspage = document.querySelector("#desktop-teams-container");
+
+    homepage.classList.add("hidden");
+    gamepage.classList.remove("hidden");
+    leaderboardpage.classList.add("hidden");
+    teamspage.classList.add("hidden");
+}
+
+function changeLeaderboard() {
+    homepage = document.querySelector("#desktop-home-container");
+    gamepage = document.querySelector("#desktop-game-container");
+    leaderboardpage = document.querySelector("#desktop-leaderboard-container");
+    teamspage = document.querySelector("#desktop-teams-container");
+
+    homepage.classList.add("hidden");
+    gamepage.classList.add("hidden");
+    leaderboardpage.classList.remove("hidden");
+    teamspage.classList.add("hidden");
+}
+
+function changeTeam() {
+    homepage = document.querySelector("#desktop-home-container");
+    gamepage = document.querySelector("#desktop-game-container");
+    leaderboardpage = document.querySelector("#desktop-leaderboard-container");
+    teamspage = document.querySelector("#desktop-teams-container");
+
+    homepage.classList.add("hidden");
+    gamepage.classList.add("hidden");
+    leaderboardpage.classList.add("hidden");
+    teamspage.classList.remove("hidden");
+}
